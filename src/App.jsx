6016bf86d93c878abb37c633e84604e64c2e60e3
rohithaider <rohithaider@gmail.com>
2024-10-24 12:49:24 +0600

    Header created and SVGs imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./Header";
+
 export default function App(){
     return (
-        <h1 className="bg-red-500">Hello Cine Rental</h1>
+        <>
+        <Header/>
+        </>
     );
 }
