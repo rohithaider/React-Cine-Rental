4ca5ae5e841c67dbfbc2b8c2a08763d1432f8605
rohithaider <rohithaider@gmail.com>
2024-10-24 13:38:35 +0600

    Movie list component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MovieList from "./cine/MovieList";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
@@ -8,6 +9,7 @@ export default function App() {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
+          <MovieList/>
         </div>
       </main>
     </>
